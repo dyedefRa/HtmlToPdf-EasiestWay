812c6137b36bcc92e11c0a8eb245d1b19eff7f3c
Abdulbaki Özturk (Nuevo) <aozturk@nuevo.com.tr>
2020-02-03 10:43:46 +0300

    2020-02-03

---
+++ Htmltopdf2/Htmltopdf2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Htmltopdf2.Models;
+using Syncfusion.HtmlConverter;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Syncfusion.Pdf;
+using System.Text;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+
+namespace Htmltopdf2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View("TEST");
+        }
+    
+      
+    }
+}
